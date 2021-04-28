63ce1cca8f3d157bb3de61e8378d0b6add5b592f
soniamajd <f.majdi2001@yahoo.com>
2021-04-28 11:18:18 -0700

    update

--- proje/proje/Ripository/IProjeRipository.cs
+++ proje/proje/Ripository/IProjeRipository.cs
@@ -9,5 +9,6 @@ namespace proje.Ripository
     interface IProjeRipository
     {
         bool Insert(string name, string family, string mobile, string email, int age, string address);
+        bool Update(int contactId, string name, string family, string mobile, string email, int age, string address);
     }
 }
