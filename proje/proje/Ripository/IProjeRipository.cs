a4a470954efe4aab715dcbdeb9daf79bd056d0d0
soniamajd <f.majdi2001@yahoo.com>
2021-04-28 11:15:13 -0700

    insert

---
+++ proje/proje/Ripository/IProjeRipository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje.Ripository
+{
+    interface IProjeRipository
+    {
+        bool Insert(string name, string family, string mobile, string email, int age, string address);
+    }
+}
