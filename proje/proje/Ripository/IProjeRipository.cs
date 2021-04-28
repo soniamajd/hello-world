0e987ed40d93356d517404b830cf7f2f1c2b50d5
soniamajd <f.majdi2001@yahoo.com>
2021-04-28 11:21:55 -0700

    delete

--- proje/proje/Ripository/IProjeRipository.cs
+++ proje/proje/Ripository/IProjeRipository.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.date
 
 namespace proje.Ripository
 {
     interface IProjeRipository
     {
+        Datatable SelectAll();
         bool Insert(string name, string family, string mobile, string email, int age, string address);
         bool Update(int contactId, string name, string family, string mobile, string email, int age, string address);
+        bool Delete (int IcontactId)
     }
 }
