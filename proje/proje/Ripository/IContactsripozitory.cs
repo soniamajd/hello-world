e9acd54fe9e723069c9f7910bb43ffcc0a71405a
soniamajd <f.majdi2001@yahoo.com>
2021-04-29 07:02:45 -0700

    Rename

--- proje/proje/Ripository/IContactsripozitory.cs
+++ proje/proje/Ripository/IContactsripozitory.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.date
+using System.Data;
 
-namespace proje.Ripository
+namespace Mycontacts.Ripository
 {
-    interface IProjeRipository
+    interface IContactsRipository
     {
-        Datatable SelectAll();
+        DataTable SelectAll();
         bool Insert(string name, string family, string mobile, string email, int age, string address);
         bool Update(int contactId, string name, string family, string mobile, string email, int age, string address);
         bool Delete (int IcontactId)
